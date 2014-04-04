11959abd631f1f39435616c4daa99bd5023c69e8
xuanfun <xuanfun@sina.com>
2014-04-04 14:03:40 +0800

    firstTime

---
+++ BasicServiceSupport.java
@@ -0,0 +1,26 @@
+
+/**   
+ * @Title: BasicServiceSupport.java
+ * @Package com.ft.core.service
+ * @Description: TODO
+ * @author hujunwen
+ * @date 2012-3-30 下午04:09:58
+ *  
+ */
+  
+package com.ft.core.service;
+
+
+
+/**
+ * Title: BasicServiceSupport 
+ * Description: BasicServiceSupport
+ * @Copyright: Copyright (c) 2011
+ * @author: hujunwen
+ * @version: 1.0
+ * @date 2012-3-30 下午04:09:58  
+ */
+
+public class BasicServiceSupport {
+
+}
